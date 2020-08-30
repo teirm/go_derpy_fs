248eb2070367be6ae64b1a9a6d20362e6301c39c
Cyrus Ramavarapu <tribunus.vii@gmail.com>
2020-08-30 19:39:42 -0400

    Updated server to use channels in a basic way and have a set of goroutines process the channels.
    Connection handling was improved and the notion of a
    header was "created" for the client data. This needs
    to be expanded upon significantly.
    
    A largely unimplemented ioHandler was also added.

--- server/server.go
+++ server/server.go
@@ -4,29 +4,133 @@ package main
 import (
 	"bufio"
 	"flag"
+	"fmt"
 	"log"
 	"net"
 )
 
 const (
-	defaultPort string = "0"
+	defaultPort       string = "0"
+	handleConnWorkers int    = 3
+	ioWorkers         int    = 3
+	respWorkers       int    = 3
+
+	GetOperation Operation = 0
 )
 
-// handle a connection and print out
-// values
-// TODO(teirm) currently very bare bones
-func handleConnection(connection net.Conn) {
+type Server struct {
+	listener net.Listener
+
+	handleChan chan net.Conn
+	ioChan     chan ClientData
+	respChan   chan int
+}
+
+type Operation uint64
+
+type ClientData struct {
+	op   Operation
+	data string
+	conn net.Conn
+}
+
+type ResponseData struct {
+	message string
+	conn    net.Conn
+}
+
+// parse the header information read from the client
+// connection
+//
+// Header Format:
+//   Operation	string
+func parseHeader(header string) (Operation, error) {
+
+	switch header {
+	case "GET":
+		log.Print("Received GET Operation\n")
+		return GetOperation, nil
+	default:
+		return 0, fmt.Errorf("invalid operation: %s", header)
+	}
+}
+
+// handle a connection and parse operation
+func handleConnection(connection net.Conn, svr Server) {
+
 	input := bufio.NewScanner(connection)
+	// parse the header for the opertion type first
+	input.Scan()
+	if err := input.Err(); err != nil {
+		log.Printf("ERROR: failed to read header: %v\n", err)
+		//TODO handle error
+	}
+
+	var clientData ClientData
+	op, err := parseHeader(input.Text())
+	if err != nil {
+		log.Printf("ERROR: unable to parse header: %v\n", err)
+		//TODO handle error
+	}
+	log.Printf("Received OP: %d\n", op)
+
+	clientData.op = op
+
 	for input.Scan() {
 		if err := input.Err(); err != nil {
 			log.Printf("ERROR: processing input: %v\n", err)
+			//TODO handle error
 			break
 		}
-		log.Println(input.Text())
+		// this is probably horribly inefficient right
+		// now but it is easy.
+		clientData.data = input.Text()
 	}
 	if err := connection.Close(); err != nil {
 		log.Printf("ERROR: unable to close connection: %v\n", err)
+		//TODO don't close connection and handle error properly
 	}
+
+	svr.ioChan <- clientData
+}
+
+// Do the IO portion
+func handleIO(data ClientData) {
+	log.Print("Unimplemented right now\n")
+	log.Print(data.data)
+}
+
+// initialize all workers for server communication
+func initServer(address string) (Server, error) {
+	var s Server
+	var err error
+
+	s.listener, err = net.Listen("tcp", address)
+	if err != nil {
+		return s, err
+	}
+
+	s.handleChan = make(chan net.Conn)
+	s.ioChan = make(chan ClientData)
+	s.respChan = make(chan int)
+
+	for i := 0; i < handleConnWorkers; i++ {
+		go func(svr Server) {
+			for conn := range svr.handleChan {
+				handleConnection(conn, s)
+			}
+		}(s)
+	}
+
+	for i := 0; i < ioWorkers; i++ {
+		go func(svr Server) {
+			for data := range svr.ioChan {
+				handleIO(data)
+			}
+		}(s)
+	}
+
+	return s, nil
 }
 
 func main() {
@@ -38,22 +142,21 @@ func main() {
 	// create address
 	address := ":" + *port
 
-	// create a server
-	listener, err := net.Listen("tcp", address)
+	serverInst, err := initServer(address)
 	if err != nil {
 		log.Fatalf("Failed to create server: %v\n", err)
 	}
 
-	log.Print("listening for connections on %s...\n", address)
+	log.Printf("listening for connections on %s...\n", address)
 	// listen for incoming connections
 	for {
-		connection, err := listener.Accept()
+		connection, err := serverInst.listener.Accept()
 		if err != nil {
 			log.Fatalf("Failed to accept connection: %v\n", err)
 		}
 
 		log.Printf("Received connection from %s\n", connection.RemoteAddr().String())
 		// spawn a go routine to handle the connection
-		go handleConnection(connection)
+		serverInst.handleChan <- connection
 	}
 }
